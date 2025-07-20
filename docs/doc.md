3946f2e516978cc926253d826870812a8d2038fc
JIOki <101073276+JIOki@users.noreply.github.com>
2025-07-20 13:31:04 -0600

    configuracion variables de entorno

---
+++ docs/doc.md
@@ -0,0 +1,6 @@
+##configurar variables de entrnorno.
+-instalar la dependencia requerida 
+   npm i --save @nestjs/config
+-crear el archivo .env
+ para crear variables de entorno , para proteger, contraseñas , keys etc
+
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { env } from 'process';    
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.development.env', // Specify the path to your .env file
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
